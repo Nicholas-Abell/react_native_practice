e88823055eeba1dd7bbec0b1f86fb0d8cd875501
Nick <darkforceyoda@gmail.com>
2024-06-21 09:47:43 -0500

    fix: typescript import error

--- app/index.jsx
+++ app/index.jsx
@@ -4,24 +4,39 @@ import { StatusBar } from "expo-status-bar";
 import { Link, Tabs } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import {images} from "../constants";
+import { images } from "../constants";
 
 const Index = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}></ScrollView>
       <View className="w-full justify-center items-center h-full px-4">
-      <Image
-            source={images.logo}
-            className="w-[130px] h-[84px]"
-            resizeMode="contain"
-          />
+        <Image
+          source={images.logo}
+          className="w-[130px] h-[84px]"
+          resizeMode="contain"
+        />
         <Image
           source={images.cards}
           className="max-w-[380px] w-full h-[300px]"
           resizeMode="contain"
         />
-        <Text>Hello</Text>
+
+        <View className="relative mt-5">
+          <Text className="text-3xl text-white font-bold text-center">
+            Discover Endless Possibilities with{" "}
+            <Text className=" text-orange-400">Aura</Text>
+          </Text>
+          <Image
+            source={images.path}
+            className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+            resizeMode="contain"
+          />
+        </View>
+        <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+          Where Creativity Meets Innovation: Embark on a Journey of Limitless
+          Exploration with Aora
+        </Text>
       </View>
     </SafeAreaView>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./app/*",],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/*",
+  ],
   theme: {
     extend: {
       colors: {
