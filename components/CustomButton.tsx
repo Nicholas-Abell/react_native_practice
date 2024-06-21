2656c40fcd749a6d118311966bc0b4dacc48b191
Nick <darkforceyoda@gmail.com>
2024-06-21 10:11:23 -0500

    custom button comp

---
+++ components/CustomButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+type CustomButtonProps = {
+  title: string;
+  handlePress: () => void;
+  containerStyles?: string;
+  textStyles?: string;
+  isLoading: boolean;
+};
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  title,
+  handlePress,
+  containerStyles,
+  textStyles,
+  isLoading,
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={handlePress}
+      activeOpacity={0.7}
+      className={`bg-orange-400 rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${
+        isLoading ? "opacity-50" : ""
+      }`}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary text-lg font-semibold ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+export default CustomButton;
